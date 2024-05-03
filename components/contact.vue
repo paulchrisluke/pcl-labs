be764a2ca14a318370a194fd504a1643aac862df
Paul Chris Luke <paulchrisluke@gmail.com>
2024-05-03 15:11:09 +0800

    thank-you

--- components/contact.vue
+++ components/contact.vue
@@ -62,14 +62,15 @@
           <SwitchLabel class="text-sm leading-6 text-gray-600">
             By selecting this, you agree to our
             {{ ' ' }}
-            <a href="#" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
+            <a href="/privacy" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
           </SwitchLabel>
         </SwitchGroup>
             </div>
             <div class="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
               <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send message</button>
             </div>
-            <input type="hidden" name="_next" value="/thank-you">
+            <input type="hidden" name="_captcha" value="false">
+            <input type="hidden" name="_next" value="https://paulchrisluke.com/thank-you">
           </form>
         </div>
       </div>
