9b0c99b630af83b54e2004d02f4adab23b051609
Paul Chris Luke <paulchrisluke@gmail.com>
2024-05-28 13:10:45 -0600

    change hero to local instead of cloudinary

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="bg-gray-900">
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
     <div class="relative isolate overflow-hidden pt-14">
       <video autoplay loop muted playsinline class="absolute inset-0 -z-10 h-full w-full object-cover bg-gray-900 opacity-40">
-        <source src="https://res.cloudinary.com/image-gallery-pcl/video/upload/v1715331109/Blawby/PCL_Labs___Agency_Video___We_Do_Stuff_online-video-cutter.com_kjuu1n.mp4" type="video/mp4">
+        <source src="/pcl-labs-agency-hero-paul-chris-luke.mp4" type="video/mp4">
       </video>
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
