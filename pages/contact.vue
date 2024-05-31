23d2997c8f8b6f77350c1a480e2afd1a84106f66
JeremieVenditti <45093125+JeremieVenditti@users.noreply.github.com>
2024-05-31 13:40:21 +0200

    About + Contact SEO meta update

--- pages/contact.vue
+++ pages/contact.vue
@@ -9,11 +9,11 @@
 useHead({
   title: 'Contact & Get a Quote',
   meta: [
-    { name: 'description', content: 'My amazing site.' },
+    { name: 'description', content: 'PCL-Labs provides full-funnel marketing and advertising from brand positioning, research, data science and full-stack development to video/photo content, and more!' },
     { property: 'og:title', content: 'Contact & Get a Quote' },  // OG title
-    { property: 'og:description', content: 'My amazing site.' },  // OG description
-    { property: 'og:image', content: 'https://example.com/path-to-your-image.jpg' },  // OG image URL
-    { name: 'keywords', content: 'Shopify, web development, marketing, SEO, PCL-Labs, Paul Chris Luke' }  // Keywords for SEO
+    { property: 'og:description', content: 'PCL-Labs provides full-funnel marketing and advertising from brand positioning, research, data science and full-stack development to video/photo content, and more!' },  // OG description
+    { property: 'og:image', content: 'PCL-contact-header.webp' },  // OG image URL
+    { name: 'keywords', content: 'Top Rated Plus Upwork Agency, CRO Focused Agency, Conversion Rate Optimization Experts, Upwork Top Rated Agency, Experienced CRO Agency, Shopify, web development, marketing, SEO, PCL-Labs, Paul Chris Luke' }  // Keywords for SEO
   ]
 })
 </script>
