ce08e60e027cc67ddf0fba40610f3e46ae1eb8ab
JeremieVenditti <45093125+JeremieVenditti@users.noreply.github.com>
2024-05-31 13:46:33 +0200

    Porfolio SEO meta change

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -13,8 +13,8 @@ useHead({
     { name: 'description', content: 'Want to increase your conversions? No problem. We help brands by blending fresh ideas with proprietary direct response tools to forge deeper brand connections and more profitable conversions.' },
     { property: 'og:title', content: 'Our Portfolio of Work' },  // OG title
     { property: 'og:description', content: 'Want to increase your conversions? No problem. We help brands by blending fresh ideas with proprietary direct response tools to forge deeper brand connections and more profitable conversions.' },  // OG description
-    { property: 'og:image', content: 'https://example.com/path-to-your-image.jpg' },  // OG image URL
-    { name: 'keywords', content: 'Shopify, web development, marketing, SEO, PCL-Labs, Paul Chris Luke' },  // Keywords for SEO
+    { property: 'og:image', content: 'PCL-about-header.webp' },  // OG image URL
+    { name: 'keywords', content: 'eCommerce CRO Specialists, UI Design Experts, UX Design Experts, Web Development Services, Paid Advertising Services, Shopify, web development, marketing, SEO, PCL-Labs, Paul Chris Luke' },  // Keywords for SEO
   ]
 })
 const { data: portfolioData } = await useAsyncData('portfolio', () =>
