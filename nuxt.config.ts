0c0e86ec6a7536162a93740c00ec31307db8124b
Paul Chris Luke <paulchrisluke@gmail.com>
2024-06-05 14:56:24 -0600

    additional seo params

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,11 @@ export default defineNuxtConfig({
   },
   site: {
     url: 'https://paulchrisluke.com', // Add your site's base URL here
+    name: 'PCL-Labs',
+    description: 'Top Rated Plus on Upwork. With over 15 years experience, PCL-Labs provides best-in-class digital and print development, design, and marketing assets to grow your business end-to-end.',
     defaultLocale: 'en',
+  },
+  seo: {
+    redirectToCanonicalSiteUrl: true
   },
 })
--- pages/about.vue
+++ pages/about.vue
@@ -14,7 +14,7 @@ useHead({
   title: 'About Us',
   meta: [
     { name: 'description', content: 'Rated in the top 1% of all agencies on Upwork. Focused on CRO, PCL-Labs creates performant, converting websites and marketing materials.' },
-    { property: 'og:title', content: 'About Us | PCL-Labs' },  // OG title
+    { property: 'og:title', content: 'About Us' },  // OG title
     { property: 'og:description', content: 'Rated in the top 1% of all agencies on Upwork. Focused on CRO, PCL-Labs creates performant, converting websites and marketing materials.' },  // OG description
     { property: 'og:image', content: 'PCL-about-header.webp' },  // OG image URL
     { name: 'keywords', content: 'Top Rated Plus Upwork Agency, CRO Focused Agency, Conversion Rate Optimization Experts, Upwork Top Rated Agency, Experienced CRO Agency, Shopify, web development, marketing, SEO, PCL-Labs, Paul Chris Luke' }  // Keywords for SEO
