af5ce4b35995363b3cff59f04167ca02d66eaee9
Paul Chris Luke <paulchrisluke@gmail.com>
2025-09-05 11:48:39 +0700

    Fix redirect loop by disabling canonical URL redirects
    - Disable redirectToCanonicalSiteUrl to prevent redirect loops
    - This was causing infinite redirects on paulchrisluke.com

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,7 +65,7 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
   },
   seo: {
-    redirectToCanonicalSiteUrl: true
+    redirectToCanonicalSiteUrl: false
   },
   ssr: true,
   nitro: {
